740c4a17390ff7e296000415ff5990fbf7834632
unknown <kai84143@gmail.com>
2022-11-14 01:39:55 +0700

    add navbar at defult

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,7 @@ const hendleSearch = () => {
 }
 </script>
 <template>
+
     <div class="  hero min-h-screen bg-accent">
         <div class="hero-content text-center ">
             <div class="max-w-md ">
@@ -47,18 +48,21 @@ const hendleSearch = () => {
                 </div>
                 <!-- <h2 class="pt-3 text-2xl font-bold">{{city.weather[0].main}}</h2> -->
                 <!-- <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> -->
-                <div class="form-control ">
-                    <div class="input-group pt-3">
-                        <input type="text" placeholder="Search…" class="input input-bordered" v-model="input" />
-                        <button class="btn btn-square " @click="hendleSearch">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                        </button>
+                <div class="flex justify-center">
+                    <div class="form-control ">
+                        <div class="input-group pt-3">
+                            <input type="text" placeholder="Search…" class="input input-bordered" v-model="input" />
+                            <button class="btn btn-square " @click="hendleSearch">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
