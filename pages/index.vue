c17c03a86a2dde8ef6468fe553d333782072a80f
unknown <kai84143@gmail.com>
2022-11-14 01:33:10 +0700

    decorate style

--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,34 @@
 const api_key = '5e393f97f096de59a3a2b8a91749363a';
 
 const cokkie = useCookie("city");
-if(! cokkie.value){
+if (!cokkie.value) {
     cokkie.value = "London"
- }
+}
 const search = ref(cokkie.value);
 const input = ref("");
 
-const { data: city, error, refresh } = useAsyncData("city", async () => {
+const { data: city, error } = useAsyncData("city", async () => {
     let response;
-    try{
-        
+    try {
         response = await $fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${api_key}`
-)
-return response
+        )
+        return response
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
-    cokkie.value = search.value;
+        cokkie.value = search.value;
 
 
     }
     return response;
-},{
-    watch:[
+}, {
+    watch: [
         search
-    ]});
+    ]
+});
 
 
-const hendleSearch = () =>{
+const hendleSearch = () => {
     search.value = input.value.trim().split(" ").join("+");
     cokkie.value = search.value;
     input.value = "";
@@ -37,28 +37,37 @@ const hendleSearch = () =>{
 }
 </script>
 <template>
-    <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">{{city.name}}  {{city.main.temp}}°</h1>
-            <!-- <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> -->
-            <div class="form-control">
-                <div class="input-group pt-3">
-                  <input type="text" placeholder="Search…" class="input input-bordered" v-model="input"/>
-                  <button class="btn btn-square " @click="hendleSearch">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                  </button>
+    <div class="  hero min-h-screen bg-accent">
+        <div class="hero-content text-center ">
+            <div class="max-w-md ">
+                <h1 class="text-5xl font-bold">{{ city.name }} {{ city.main.temp }}°</h1>
+                <div class="flex justify-center">
+                    <img :src="`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`" alt="">
+
                 </div>
-              </div>
-          </div>
+                <!-- <h2 class="pt-3 text-2xl font-bold">{{city.weather[0].main}}</h2> -->
+                <!-- <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> -->
+                <div class="form-control ">
+                    <div class="input-group pt-3">
+                        <input type="text" placeholder="Search…" class="input input-bordered" v-model="input" />
+                        <button class="btn btn-square " @click="hendleSearch">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+    </div>
 
 </template>
 <style scoped>
-.container{
+.container {
     height: 100vh;
     margin: 0 auto;
-    
+
 }
 </style>
